9c46a64bcc6289cbc1cd7b714fb11a53d706a99e
Павел Трифонов <p4veltr@gmail.com>
2023-04-13 23:05:43 +0300

    Create table fix

--- 1-CreateTable.sql
+++ 1-CreateTable.sql
@@ -1,8 +1,8 @@
 CREATE TABLE PERSONS (
-	name varchar(250),
-	surname varchar(250),
-	age int,
-	phone_number numeric(11,0),
-	city_of_living varchar(500),
+	name varchar(250) NOT NULL,
+	surname varchar(250) NOT NULL,
+	age int NOT NULL,
+	phone_number numeric(11,0) NOT NULL,
+	city_of_living varchar(500) NOT NULL,
 	CONSTRAINT "pKey" PRIMARY KEY (name, surname, age)
 )
